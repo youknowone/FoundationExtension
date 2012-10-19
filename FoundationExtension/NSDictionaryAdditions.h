cb9a36b74b054fd86b44d64b3f15fe357664879f
Jeong YunWon <jeong@youknowone.org>
2012-10-19 10:33:34 +0900

    Fix initWithData related signatures. Add headerdoc.

--- FoundationExtension/NSDictionaryAdditions.h
+++ FoundationExtension/NSDictionaryAdditions.h
@@ -8,16 +8,76 @@
 
 @interface NSDictionary (FoundationExtension)
 
+/*!
+ *  @brief Initializes a newly allocated dictionary with the contents of data. (plist)
+ *  @param data
+ *      An NSData object containing bytes of plist file representation.
+ *  @return A dictionary initialized to contain the contents in data. Returns nil if the contents of the request can’t be parsed into a dictionary. The returned object might be different than the original receiver.
+ *  @availability Available in OS X v10.6 and later.
+ *  @seealso -initWithData:options:format:error:
+ *  @seealso +dictionaryWithData:options:format:error:
+ */
 - (id)initWithData:(NSData *)data;
+/*!
+ *  @brief Creates and returns a dictionary containing the contents of data. (plist)
+ *  @availability Available in OS X v10.6 and later.
+ *  @see -initWithData:
+ */
 + (id)dictionaryWithData:(NSData *)data;
 
-- (id)initWithData:(NSData *)data options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error;// NS_AVAILABLE(10_6, 4_0);
-+ (id)dictionaryWithData:(NSData *)data options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error;// NS_AVAILABLE(10_6, 4_0);
+/*!
+ *  @brief Initializes a newly allocated dictionary with the contents of data. (plist)
+ *  @param data
+ *      An NSData object containing bytes of plist file representation.
+ *  @param format
+ *      A property list format. Possible values for format are described in @link NSPropertyListFormat @/link.
+ *  @param error
+ *      If the method does not complete successfully, upon return contains an NSError object that describes the problem.
+ *  @return A dictionary initialized to contain the contents in data. Returns nil if the contents of the request can’t be parsed into a dictionary. The returned object might be different than the original receiver.
+ *  @availability Available in OS X v10.6 and later.
+ *  @seealso -initWithData:
+ *  @seealso +dictionaryWithData:
+ */
+- (id)initWithData:(NSData *)data format:(NSPropertyListFormat *)format error:(NSError **)error;
+/*!
+ *  @brief Creates and returns a dictionary containing the contents of data. (plist)
+ *  @availability Available in OS X v10.6 and later.
+ *  @see -initWithData:options:format:error:
+ */
++ (id)dictionaryWithData:(NSData *)data format:(NSPropertyListFormat *)format error:(NSError **)error;
 
+/*!
+ *  @brief Initializes a newly allocated dictionary with the contents of the result specified by a given request.
+ *  @param request
+ *      The URLRequest of a file containing a string representation of a dictionary produced by the @link writeToURL:atomically: @/link method.
+ *  @return A dictionary initialized to contain the contents specified by request. Returns nil if the location can’t be opened or if the contents of the request can’t be parsed into a dictionary. The returned object might be different than the original receiver.
+ *  @seealso -initWithContentsOfURLRequest:format:error:
+ *  @seealso +dictionaryWithContentsOfURLRequest:format:error:
+ */
 - (id)initWithContentsOfURLRequest:(NSURLRequest *)request;
+/*!
+ *  @brief Creates and returns a dictionary containing the contents of the result specified by a given request.
+ *  @see initWithContentsOfURLRequest:
+ */
 + (id)dictionaryWithContentsOfURLRequest:(NSURLRequest *)request;
 
-- (id)initWithContentsOfURLRequest:(NSURLRequest *)request options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error;
-+ (id)dictionaryWithContentsOfURLRequest:(NSURLRequest *)request options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error;
+/*!
+ *  @brief Initializes a newly allocated dictionary with the contents of the result specified by a given request.
+ *  @param request
+ *      The URLRequest of a file containing a string representation of a dictionary produced by the @link writeToURL:atomically: @/link method.
+ *  @param format
+ *      A property list format. Possible values for format are described in @link NSPropertyListFormat @/link.
+ *  @param error
+ *      If the method does not complete successfully, upon return contains an NSError object that describes the problem.
+ *  @result A dictionary initialized to contain the contents specified by request. Returns nil if the location can’t be opened or if the contents of the request can’t be parsed into a dictionary. The returned object might be different than the original receiver.
+ *  @seealso -initWithContentsOfURLRequest:
+ *  @seealso +dictionaryWithContentsOfURLRequest:
+ */
+- (id)initWithContentsOfURLRequest:(NSURLRequest *)request format:(NSPropertyListFormat *)format error:(NSError **)error;
+/*!
+ *  @brief Creates and returns a dictionary containing the contents of the result specified by a given request.
+ *  @see initWithContentsOfURLRequest:format:error:
+ */
++ (id)dictionaryWithContentsOfURLRequest:(NSURLRequest *)request format:(NSPropertyListFormat *)format error:(NSError **)error;
 
 @end
--- FoundationExtension/NSDictionaryAdditions.m
+++ FoundationExtension/NSDictionaryAdditions.m
@@ -12,19 +12,19 @@
 
 @implementation NSDictionary (FoundationExtension)
 
-- (id) initWithData:(NSData *)data {
-    return [self initWithData:data options:0 format:NULL error:NULL];
+- (id)initWithData:(NSData *)data {
+    return [self initWithData:data format:NULL error:NULL];
 }
 
-+ (id) dictionaryWithData:(NSData *)data {
++ (id)dictionaryWithData:(NSData *)data {
     return [[[self alloc] initWithData:data] autorelease];
 }
 
-- (id) initWithData:(NSData *)data options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error {
+- (id)initWithData:(NSData *)data format:(NSPropertyListFormat *)format error:(NSError **)error {
     NSDictionary *contents;
     //TODO: check deployment version
     if ( [NSPropertyListSerialization respondsToSelector:@selector(propertyListWithData:options:format:error:)] ) {
-        contents = [NSPropertyListSerialization propertyListWithData:data options:opt format:format error:error];
+        contents = [NSPropertyListSerialization propertyListWithData:data options:0 format:format error:error];
     } else { // support os < osx10.6 or ios4.0
         contents = [NSPropertyListSerialization propertyListFromData:data mutabilityOption:NSPropertyListImmutable format:NULL errorDescription:NULL];
     }
@@ -35,29 +35,29 @@
     return [self initWithDictionary:contents];
 }
 
-+ (id) dictionaryWithData:(NSData *)data options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error {
-    return [[[self alloc] initWithData:data options:opt format:format error:error] autorelease];
++ (id)dictionaryWithData:(NSData *)data format:(NSPropertyListFormat *)format error:(NSError **)error {
+    return [[[self alloc] initWithData:data format:format error:error] autorelease];
 }
 
-- (id) initWithContentsOfURLRequest:(NSURLRequest *)request {
-    return [self initWithContentsOfURLRequest:request options:0 format:NULL error:NULL];
+- (id)initWithContentsOfURLRequest:(NSURLRequest *)request {
+    return [self initWithContentsOfURLRequest:request format:NULL error:NULL];
 }
 
-+ (id) dictionaryWithContentsOfURLRequest:(NSURLRequest *)request {
-    return [[[self alloc] initWithContentsOfURLRequest:request options:0 format:NULL error:NULL] autorelease];
++ (id)dictionaryWithContentsOfURLRequest:(NSURLRequest *)request {
+    return [[[self alloc] initWithContentsOfURLRequest:request format:NULL error:NULL] autorelease];
 }
 
-- (id) initWithContentsOfURLRequest:(NSURLRequest *)request options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error {
+- (id)initWithContentsOfURLRequest:(NSURLRequest *)request format:(NSPropertyListFormat *)format error:(NSError **)error {
     NSData *data = [NSData dataWithContentsOfURLRequest:request error:error];
     if (data == nil) {
         [self release];
         return nil;
     }
-    return [self initWithData:data options:opt format:format error:error];
+    return [self initWithData:data format:format error:error];
 }
 
-+ (id) dictionaryWithContentsOfURLRequest:(NSURLRequest *)request options:(NSPropertyListReadOptions)opt format:(NSPropertyListFormat *)format error:(NSError **)error {
-    return [[[self alloc] initWithContentsOfURLRequest:request options:opt format:format error:error] autorelease];
++ (id)dictionaryWithContentsOfURLRequest:(NSURLRequest *)request format:(NSPropertyListFormat *)format error:(NSError **)error {
+    return [[[self alloc] initWithContentsOfURLRequest:request format:format error:error] autorelease];
 }
 
 @end

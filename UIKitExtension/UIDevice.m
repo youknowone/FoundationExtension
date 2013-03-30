e59a3a36ce476c4476c45180aa17a883183d90b1
Jeong YunWon <jeong@youknowone.org>
2013-03-31 01:50:10 +0900

    simplify deprecated UIDeviceType implementation

--- UIKitExtension/UIDevice.m
+++ UIKitExtension/UIDevice.m
@@ -22,37 +22,14 @@
     return [[UIDevice currentDevice] uniqueIdentifier];
 }
 
-UIADeviceType _deviceType;
-+ (UIADeviceType) currentDeviceType {
-    return _deviceType;
-}
-
-+ (void)initialize {
-    if ( self == [UIDevice class] ) {
-        // FIXME: use device constant to define these
-        UIDevice *currentDevice = [UIDevice currentDevice];
-        if ( [[currentDevice model] isEqualToString:@"iPhone"] ) {
-            _deviceType = UIADeviceTypePhone;
-        } else if ( [[currentDevice model] isEqualToString:@"iPod touch"] ) {
-            _deviceType = UIADeviceTypePod;
-        } else if ( [[currentDevice model] isEqualToString:@"iPad"] ) {
-            _deviceType = UIADeviceTypePad;
-        } else {
-            _deviceType = UIADeviceTypeUnknown;
-            NSLog(@"failed to detect device!!");
-            // try again
-            CGFloat screenWidth = [UIScreen mainScreen].applicationFrame.size.width;
-            dlog(TRUE, @"check device resolution: %.0f", screenWidth);
-            if ( screenWidth == 320.0f ) {
-                _deviceType = UIADeviceTypePhone;
-            } else if ( screenWidth == 768.0f ) {
-                _deviceType = UIADeviceTypePad;
-            } else {
-                _deviceType = UIADeviceTypeUnknown;
-                dassert(NO);
-            }
-        }
++ (UIADeviceType)currentDeviceType {
+    switch ([[self currentDevice] userInterfaceIdiom]) {
+        case UIUserInterfaceIdiomPad:
+            return UIADeviceTypePad;
+        default:
+            break;
     }
+    return UIADeviceTypePhone;
 }
 
 - (NSString *)uniqueToken {

de934f28ad41dfb310a04f95d5cbe18bf8a4dfee
Jeong YunWon <jeong@youknowone.org>
2013-04-12 00:55:30 +0900

    push verbose trivial methods to internal category

--- UIKitExtension/UIColor.m
+++ UIKitExtension/UIColor.m
@@ -7,6 +7,8 @@
 //
 
 #import "NSString.h"
+#import "NSDictionary.h"
+
 #import "UIColor.h"
 
 
@@ -46,9 +48,87 @@
 @end
 
 
+@interface UIColor (HTMLColorInternal)
+
+/*!
+ *  @brief Initialize with html color code
+ *  @details This accept formats as like "#fff".
+ */
+- (UIColor *)initWithHTMLHexExpression16:(NSString *)code;
+/*!
+ *  @brief Initialize with html color code
+ *  @details This accept formats as like "#0f0f0f"
+ */
+- (UIColor *)initWithHTMLHexExpression32:(NSString *)code;
+/*!
+ *  @brief Initialize with html color code
+ *  @details This accept formats as like "#dddf". Last character is alpha channel.
+ */
+- (UIColor *)initWithHTMLHexExpression16a:(NSString *)code;
+/*!
+ *  @brief Initialize with html color code
+ *  @details This accept formats as like "#ddddddff". Last 2 characters are alpha channel.
+ */
+- (UIColor *)initWithHTMLHexExpression32a:(NSString *)code;
+/*!
+ *  @brief Creates and return color object with html color code
+ *  @details This accept formats as like "#fff".
+ */
++ (UIColor *)colorWithHTMLHexExpression16:(NSString *)code;
+/*!
+ *  @brief Creates and return color object with html color code
+ *  @details This accept formats as like "#0f0f0f".
+ */
++ (UIColor *)colorWithHTMLHexExpression32:(NSString *)code;
+/*!
+ *  @brief Creates and return color object with html color code
+ *  @details This accept formats as like "#dddf". Last character is alpha channel.
+ */
++ (UIColor *)colorWithHTMLHexExpression16a:(NSString *)code;
+/*!
+ *  @brief Creates and return color object with html color code
+ *  @details This accept formats as like "#ddddddff". Last 2 characters are alpha channel.
+ */
++ (UIColor *)colorWithHTMLHexExpression32a:(NSString *)code;
+
+/*!
+ *  @brief Return constant color object for given color name.
+ *  @details Available color names are standard HTML color names, "orange" for css and "transperent" for clear color.
+ */
++ (UIColor *)colorWithHTMLColorName:(NSString *)name;
+
+@end
+
+
+@implementation UIColor (HTMLColorInternal)
+
 NSDictionary *FoundationExtensionUIColorHTMLColorTable = nil;
 
-@implementation UIColor (HTMLColor)
++ (void)initialize {
+    if (self == [UIColor class]) {
+        FoundationExtensionUIColorHTMLColorTable =
+        [[NSDictionary alloc] initWithObjectsAndKeys:
+         [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.0f], @"transperent",
+         [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:1.0f], @"black",
+         [UIColor colorWithRed:0.75f green:0.75f blue:0.75f alpha:1.0f], @"silver",
+         [UIColor colorWithRed:0.5f green:0.0f blue:0.0f alpha:1.0f], @"maroon",
+         [UIColor colorWithRed:1.0f green:0.0f blue:0.0f alpha:1.0f], @"red",
+         [UIColor colorWithRed:0.0f green:0.0f blue:0.5f alpha:1.0f], @"navy",
+         [UIColor colorWithRed:0.0f green:0.0f blue:1.0f alpha:1.0f], @"blue",
+         [UIColor colorWithRed:0.5f green:0.0f blue:0.5f alpha:1.0f], @"purple",
+         [UIColor colorWithRed:1.0f green:0.0f blue:1.0f alpha:1.0f], @"fuchsia",
+         [UIColor colorWithRed:0.0f green:0.5f blue:0.0f alpha:1.0f], @"green",
+         [UIColor colorWithRed:0.0f green:1.0f blue:0.0f alpha:1.0f], @"lime",
+         [UIColor colorWithRed:0.5f green:0.5f blue:0.0f alpha:1.0f], @"olive",
+         [UIColor colorWithRed:1.0f green:1.0f blue:0.0f alpha:1.0f], @"yellow",
+         [UIColor colorWithRed:0.0f green:0.5f blue:0.5f alpha:1.0f], @"teal",
+         [UIColor colorWithRed:0.0f green:1.0f blue:1.0f alpha:1.0f], @"aqua",
+         [UIColor colorWithRed:0.5f green:0.5f blue:0.5f alpha:1.0f], @"gray",
+         [UIColor colorWithRed:1.0f green:1.0f blue:1.0f alpha:1.0f], @"white",
+         [UIColor colorWithHTMLHexExpression32:@"#ffa500"], @"orange",
+         nil];
+    }
+}
 
 - (UIColor *)initWithHTMLHexExpression16:(NSString *)code {
     return [self initWithRed:[[code substringFromIndex:1 length:1] hexadecimalValue]/15.0f
@@ -78,49 +158,8 @@ NSDictionary *FoundationExtensionUIColorHTMLColorTable = nil;
                        alpha:[[code substringFromIndex:7 length:2] hexadecimalValue]/255.0f];
 }
 
-- (UIColor *)initWithHTMLExpression:(NSString *)code {
-    if (![code hasPrefix:@"#"]) {
-        [self release];
-        return [[[self class] colorWithHTMLColorName:code] retain];
-    }
-    switch (code.length) {
-        case 4: return [self initWithHTMLHexExpression16:code];
-        case 5: return [self initWithHTMLHexExpression16a:code];
-        case 7: return [self initWithHTMLHexExpression32:code];
-        case 9: return [self initWithHTMLHexExpression32a:code];
-    }
-    [self release];
-    return nil;
-}
-
-+ (void)initialize {
-    if (self == [UIColor class]) {
-        FoundationExtensionUIColorHTMLColorTable =
-        [[NSDictionary alloc] initWithObjectsAndKeys:
-         [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.0f], @"transperent",
-         [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:1.0f], @"black",
-         [UIColor colorWithRed:0.75f green:0.75f blue:0.75f alpha:1.0f], @"silver",
-         [UIColor colorWithRed:0.5f green:0.0f blue:0.0f alpha:1.0f], @"maroon",
-         [UIColor colorWithRed:1.0f green:0.0f blue:0.0f alpha:1.0f], @"red",
-         [UIColor colorWithRed:0.0f green:0.0f blue:0.5f alpha:1.0f], @"navy",
-         [UIColor colorWithRed:0.0f green:0.0f blue:1.0f alpha:1.0f], @"blue",
-         [UIColor colorWithRed:0.5f green:0.0f blue:0.5f alpha:1.0f], @"purple",
-         [UIColor colorWithRed:1.0f green:0.0f blue:1.0f alpha:1.0f], @"fuchsia",
-         [UIColor colorWithRed:0.0f green:0.5f blue:0.0f alpha:1.0f], @"green",
-         [UIColor colorWithRed:0.0f green:1.0f blue:0.0f alpha:1.0f], @"lime",
-         [UIColor colorWithRed:0.5f green:0.5f blue:0.0f alpha:1.0f], @"olive",
-         [UIColor colorWithRed:1.0f green:1.0f blue:0.0f alpha:1.0f], @"yellow",
-         [UIColor colorWithRed:0.0f green:0.5f blue:0.5f alpha:1.0f], @"teal",
-         [UIColor colorWithRed:0.0f green:1.0f blue:1.0f alpha:1.0f], @"aqua",
-         [UIColor colorWithRed:0.5f green:0.5f blue:0.5f alpha:1.0f], @"gray",
-         [UIColor colorWithRed:1.0f green:1.0f blue:1.0f alpha:1.0f], @"white",
-         [UIColor colorWithHTMLHexExpression32:@"#ffa500"], @"orange",
-         nil];
-    }
-}
-
 + (UIColor *)colorWithHTMLColorName:(NSString *)name {
-    return [FoundationExtensionUIColorHTMLColorTable objectForKey:name];
+    return [FoundationExtensionUIColorHTMLColorTable :name];
 }
 
 + (UIColor *)colorWithHTMLHexExpression16:(NSString *)code {
@@ -137,6 +176,26 @@ NSDictionary *FoundationExtensionUIColorHTMLColorTable = nil;
 
 + (UIColor *)colorWithHTMLHexExpression32a:(NSString *)code {
     return [[[self alloc] initWithHTMLHexExpression32a:code] autorelease];
+}
+
+@end
+
+
+@implementation UIColor (HTMLColor)
+
+- (UIColor *)initWithHTMLExpression:(NSString *)code {
+    if (![code hasPrefix:@"#"]) {
+        [self release];
+        return [[[self class] colorWithHTMLColorName:code] retain];
+    }
+    switch (code.length) {
+        case 4: return [self initWithHTMLHexExpression16:code];
+        case 5: return [self initWithHTMLHexExpression16a:code];
+        case 7: return [self initWithHTMLHexExpression32:code];
+        case 9: return [self initWithHTMLHexExpression32a:code];
+    }
+    [self release];
+    return nil;
 }
 
 + (UIColor *)colorWithHTMLExpression:(NSString *)code {

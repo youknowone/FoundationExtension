958cc17f23354c0f450b4c0038c2520c4e6668e2
Jeong YunWon <jeong@youknowone.org>
2013-04-24 17:04:33 +0900

    add controll property shortcut

---
+++ CocoaExtension/NSControl.h
@@ -0,0 +1,13 @@
+//
+//  NSControl.h
+//  FoundationExtension
+//
+//  Created by Jeong YunWon on 13. 4. 24..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+@interface NSControl (Properties)
+
+@property(nonatomic, assign, getter=isEnabled, setter=setEnabled:) BOOL enabled;
+
+@end
---
+++ CocoaExtension/NSControl.m
@@ -0,0 +1,15 @@
+//
+//  NSControl.m
+//  FoundationExtension
+//
+//  Created by Jeong YunWon on 13. 4. 24..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import "NSControl.h"
+
+@implementation NSControl (Properties)
+@dynamic enabled;
+
+
+@end
